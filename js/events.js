88bbae6338043739b4329e8a2dceeba0afce82e6
Paul Calnan <pcalnan71@gmail.com>
2021-04-09 04:46:47 +1000

    Latest version, clean up code, added comments

--- js/events.js
+++ js/events.js
@@ -1,21 +1,26 @@
 
 
 $(document).ready(function() {
-
-
-
+  //Listens for click from current player
+  //Also checks if any win message is present, if so, ends the game
   $('.square').on('click', function () {
-    if ($('div').parent(logo)) {
-    $(this).off('click')
+    if (winnerMessage !== '') {
+      return; // Game over
+    } else { // If no win, message next player turn
+      $('#next').text('Next player turn - '+ nextPlayer);
+    }
+    if ($('div').parent(logo)) {//Checks if a css class applied via click
+      $(this).off('click');//if so, disables ability to reclick that square
+      turnSwap(player);//Calls function to swap to next player
     };
-  let squareChoice = $(this).attr('id');
-  $(this).addClass(logo);
 
-  $('#winner').text(winnerMessage);
+    let squareValue = $(this).attr('id');//assigns clicked square ID variable
+      $(this).addClass(logo);//Adds 'O' or 'X' to the clicked square for current player
+      square(player, squareValue);//calls function in player.js to start sort of player inputs
+      $('#winner').text(winnerMessage);//receives a win message from player.js if win match
+  });
 
-
-
-  turnSwap(player);
-  square(player, squareChoice);
+  $('#reset').click( function () {//resets whole game at this time. local variables to follow soon.
+    location.reload();
   });
 });
--- js/players.js
+++ js/players.js
@@ -1,62 +1,66 @@
-
 let win = {
-'win1' : ['1', '2', '3'],
-'win2' : ['4', '5', '6'],
-'win3' : ['7', '8', '9'],
-'win4' : ['1', '4', '7'],
-'win5' : ['2', '5', '8'],
-'win6' : ['3', '6', '9'],
-'win7' : ['1', '5', '9'],
-'win8' : ['3', '5', '7'],
+  'win1' : ['1', '2', '3'],
+  'win2' : ['4', '5', '6'],
+  'win3' : ['7', '8', '9'],
+  'win4' : ['1', '4', '7'],
+  'win5' : ['2', '5', '8'],
+  'win6' : ['3', '6', '9'],
+  'win7' : ['1', '5', '9'],
+  'win8' : ['3', '5', '7'],
 };
-
-
-let player1 =[];
-let player2 =[];
+// Initialise all variables
+let draw = [];
+let player1 = [];
+let player1Score = 0;
+let player2Score = 0;
+let player2 = [];
 let winnerMessage = '';
-let logo = 'red'
-let player = 0
+let nextPlayer = 'O';
+let logo = 'cross'
+let player = 1
 
-const turnSwap = function(nextPlayer) {
+const turnSwap = function(currentPlayer) { //Function to swap current player to next
   if ( player === 1 ) {
-    logo = 'red';
+    logo = 'cross';
     player = 0;
   } else {
-    logo = 'blue';
+    logo = 'naught';
     player = 1;
   }
-return player;
+  return player;
 };
-
-const square = function(player, value) {
-
-  if (player === 1) {
-    player1.push(value);
-    //console.log(player1);
-  } else {
-    player2.push(value);
-  }
-
-  winCombo = Object.values(win);
-  for ( let i = 0; i < winCombo.length; i++) {
-  const winner = winCombo[i];
-  compare(player1, winner);
-  compare(player2, winner);
-
-  if (compare(player1.sort(), winner.sort())) {
-    return winnerMessage = ('Player 1 wins!') ;
-  } else if (compare(player2.sort(), winner.sort())) {
-    return winnerMessage = ('Player 2 wins!') ;
-  } else if ((player1.length > 4)  || (player2.length > 4))  {
-    return winnerMessage = ('DRAW');
-    break;
+//square function receives the players square ID and pushes that ID into the players array
+const square = function(player, squareValue) {
+  draw.push(squareValue); //Pushes all square ID's into an array and calls a draw if the array length = 9 available squares
+  if (draw.length === 9) {
+    winnerMessage = ('DRAW!');
+    $('#next').text('Click reset to start over');
   };
-}
+  if (player === 0) { //Pushes square value to array
+    player1.push(squareValue);
+    nextPlayer = ('X'); //jquery takes next player message
+  } else {
+    player2.push(squareValue);
+    nextPlayer = ('O');
+  };
+  winCompare(player1, player2);  //Pass current arrays for comparison
 };
+//winCompare function takes in both players arrays for comparison
+const winCompare = function(player1Array, player2Array) {
+  winCombo = Object.values(win); //Iterate winning combination array object and assign each to a variable
+  for ( let i = 0; i < winCombo.length; i++) {
+    let winner = winCombo[i];
+    const player1Wins = winner.every(val => player1Array.includes(val)); //Checks if entire player array has matching elements within win combinations
+    const player2Wins = winner.every(val => player2Array.includes(val));
 
-const compare = function(a, b) {
-
-  return Array.isArray(a) && Array.isArray(b) && a.length === b.length && a.every((val, index) => val === b[index]);
-
-return compare;
+    if (player1Wins) {
+      winnerMessage = ('Player 1 wins!');
+      player1Score = player1Score + 1;
+      $('#next').text('Click reset to start over');
+    } else if (player2Wins) {
+      winnerMessage = ('Player 2 wins!');
+      player2Score = player2Score + 1;
+      $('#next').text('Click reset to start over');
+    }
+  }
 };
